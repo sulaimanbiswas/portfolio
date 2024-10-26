19946fb04efc88ff9880e425cd9b5e7de6b83c43
Md Suliman <Sulaimanbiswasbd@gmail.com>
2024-10-26 22:02:34 +0600

    Refactor package dependencies to include @react-three/drei and @react-three/fiber

--- src/app/(home)/components/HeroSection.tsx
+++ src/app/(home)/components/HeroSection.tsx
@@ -1,40 +1,94 @@
 import { Button } from "@/components/ui/button";
-import { Facebook } from "lucide-react";
+import WordCloud3D from "@/components/word-cloud-3d/WordCloud3D";
+import { Facebook, Github, Instagram, Linkedin, Youtube } from "lucide-react";
 import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <div className="relative flex h-screen w-full items-center justify-center bg-white bg-dot-black/[0.2] dark:bg-black dark:bg-dot-white/[0.2]">
+    <div className="relative flex h-screen w-full items-center justify-evenly bg-white bg-dot-black/[0.2] dark:bg-black dark:bg-dot-white/[0.2]">
       <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
-      <div className="grid grid-cols-2 gap-5">
-        <div className="col-span-2 flex flex-col items-start justify-center gap-4 md:col-span-1">
-          <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text text-2xl font-bold text-transparent sm:text-2xl">
-            Hello, I&apos;m
-          </p>
-          <p className="z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text text-4xl font-bold text-transparent sm:text-7xl">
-            Md Suliman
-          </p>
-          <p className="z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text text-lg text-transparent sm:text-2xl">
-            Front-end Web Developer
-          </p>
-          {/* Icon  */}
-          <div className="flex gap-4">
-            <Link href="">
-              <Button size="icon" variant="outline">
-                <Facebook />
-              </Button>
-            </Link>
-          </div>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-2 gap-5">
+          <div className="col-span-2 flex flex-col items-start justify-center gap-4 md:col-span-1">
+            <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text text-2xl font-bold text-transparent sm:text-2xl">
+              Hello, I&apos;m
+            </p>
+            <p className="z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text text-4xl font-bold text-transparent sm:text-7xl">
+              Md Suliman
+            </p>
+            <p className="z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text text-lg text-transparent sm:text-2xl">
+              Front-end Web Developer
+            </p>
 
-          <div className="flex gap-4">
-            <Button className="relative inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#ffffff,45%,#d4e4f0,55%,#ffffff)] bg-[length:200%_100%] px-6 font-medium text-slate-950 transition-colors dark:bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] dark:text-slate-400">
-              Hire me
-              <span className="absolute inset-x-0 -bottom-px mx-auto h-px w-1/2 bg-gradient-to-r from-transparent via-blue-500 to-transparent" />
-            </Button>
-            <Button className="relative inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#ffffff,45%,#d4e4f0,55%,#ffffff)] bg-[length:200%_100%] px-6 font-medium text-slate-950 transition-colors dark:bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] dark:text-slate-400">
-              Resume
-              <span className="absolute inset-x-0 -bottom-px mx-auto h-px w-1/2 bg-gradient-to-r from-transparent via-blue-500 to-transparent" />
-            </Button>
+            {/* Icon  */}
+            <div className="flex gap-4">
+              <Link href="">
+                <Button size="icon" variant="outline" className="relative">
+                  <Github />
+                  <span className="absolute inset-x-0 -bottom-px mx-auto h-px w-1/2 bg-gradient-to-r from-transparent via-blue-500 to-transparent" />
+                </Button>
+              </Link>
+              <Link href="">
+                <Button size="icon" variant="outline" className="relative">
+                  <Linkedin />
+                  <span className="absolute inset-x-0 -bottom-px mx-auto h-px w-1/2 bg-gradient-to-r from-transparent via-blue-500 to-transparent" />
+                </Button>
+              </Link>
+              <Link href="">
+                <Button size="icon" variant="outline" className="relative">
+                  <Facebook />
+                  <span className="absolute inset-x-0 -bottom-px mx-auto h-px w-1/2 bg-gradient-to-r from-transparent via-blue-500 to-transparent" />
+                </Button>
+              </Link>
+              <Link href="">
+                <Button size="icon" variant="outline" className="relative">
+                  <Instagram />
+                  <span className="absolute inset-x-0 -bottom-px mx-auto h-px w-1/2 bg-gradient-to-r from-transparent via-blue-500 to-transparent" />
+                </Button>
+              </Link>
+              <Link href="">
+                <Button size="icon" variant="outline" className="relative">
+                  <Youtube />
+                  <span className="absolute inset-x-0 -bottom-px mx-auto h-px w-1/2 bg-gradient-to-r from-transparent via-blue-500 to-transparent" />
+                </Button>
+              </Link>
+            </div>
+
+            <div className="flex gap-4">
+              <Button className="relative inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#ffffff,45%,#d4e4f0,55%,#ffffff)] bg-[length:200%_100%] px-6 font-medium text-slate-950 transition-colors dark:bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] dark:text-slate-400">
+                Hire me
+                <span className="absolute inset-x-0 -bottom-px mx-auto h-px w-1/2 bg-gradient-to-r from-transparent via-blue-500 to-transparent" />
+              </Button>
+              <Button className="relative inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#ffffff,45%,#d4e4f0,55%,#ffffff)] bg-[length:200%_100%] px-6 font-medium text-slate-950 transition-colors dark:bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] dark:text-slate-400">
+                Resume
+                <span className="absolute inset-x-0 -bottom-px mx-auto h-px w-1/2 bg-gradient-to-r from-transparent via-blue-500 to-transparent" />
+              </Button>
+            </div>
+          </div>
+          <div className="col-span-2 hidden w-full md:col-span-1 md:block md:h-full lg:h-[500px] xl:h-[600px]">
+            <WordCloud3D
+              customWords={[
+                "HTML",
+                "CSS",
+                "JavaScript",
+                "TypeScript",
+                "React",
+                "Next.js",
+                "TailwindCSS",
+                "Bootstrap",
+                "Node.js",
+                "Express.js",
+                "MongoDB",
+                "Firebase",
+                "Git",
+                "GitHub",
+                "VS Code",
+                "Figma",
+                "Adobe XD",
+                "Photoshop",
+                "Illustrator",
+              ]}
+            />
           </div>
         </div>
       </div>
---
+++ src/components/word-cloud-3d/WordCloud3D.tsx
@@ -0,0 +1,135 @@
+"use client";
+
+import { Billboard, Text, TrackballControls } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useTheme } from "next-themes";
+import { generate } from "random-words";
+import { Suspense, useEffect, useMemo, useRef, useState } from "react";
+import * as THREE from "three";
+
+interface WordProps {
+  children: string;
+  position: THREE.Vector3;
+}
+
+function Word({ children, position }: WordProps) {
+  const { resolvedTheme } = useTheme();
+
+  const color = new THREE.Color();
+  const fontProps = {
+    font: "/fonts/GeistMonoVF.woff",
+    fontSize: 2.5,
+    letterSpacing: -0.05,
+    lineHeight: 1,
+    "material-toneMapped": false,
+  };
+  const ref = useRef<THREE.Mesh>(null);
+  const [hovered, setHovered] = useState(false);
+
+  const over = (e: any) => (e.stopPropagation(), setHovered(true));
+  const out = () => setHovered(false);
+
+  useEffect(() => {
+    document.body.style.cursor = hovered ? "pointer" : "auto";
+  }, [hovered]);
+
+  useFrame(() => {
+    if (
+      ref.current &&
+      ref.current.material instanceof THREE.MeshBasicMaterial
+    ) {
+      ref.current.material.color.lerp(
+        color.set(
+          hovered
+            ? "#3b82f6"
+            : resolvedTheme === "dark"
+              ? "#ffffff"
+              : "#000000",
+        ),
+        0.1,
+      );
+    }
+  });
+
+  return (
+    <Billboard position={position}>
+      <Text ref={ref} onPointerOver={over} onPointerOut={out} {...fontProps}>
+        {children}
+      </Text>
+    </Billboard>
+  );
+}
+
+interface CloudProps {
+  count?: number;
+  radius?: number;
+  customWords?: string[];
+}
+
+function Cloud({ count = 4, radius = 15, customWords }: CloudProps) {
+  const words = useMemo(() => {
+    const temp: [THREE.Vector3, string][] = [];
+    const spherical = new THREE.Spherical();
+    const phiSpan = Math.PI / (count + 1);
+    const thetaSpan = (Math.PI * 2) / count;
+    for (let i = 1; i < count + 1; i++) {
+      for (let j = 0; j < count; j++) {
+        const word = customWords
+          ? customWords[(i * count + j) % customWords.length]
+          : generate();
+        temp.push([
+          new THREE.Vector3().setFromSpherical(
+            spherical.set(radius, phiSpan * i, thetaSpan * j),
+          ),
+          word as string,
+        ]);
+      }
+    }
+    return temp;
+  }, [count, radius, customWords]);
+
+  return (
+    <>
+      {words.map(([pos, word], index) => (
+        <Word key={index} position={pos}>
+          {word}
+        </Word>
+      ))}
+    </>
+  );
+}
+
+function RotatingGroup({ count, radius, customWords }: CloudProps) {
+  const groupRef = useRef<THREE.Group>(null);
+
+  useFrame(() => {
+    if (groupRef.current) {
+      // Adjust the rotation speed by modifying the increment values
+      groupRef.current.rotation.y += 0.002;
+      groupRef.current.rotation.x += 0.001;
+    }
+  });
+
+  return (
+    <group ref={groupRef}>
+      <Cloud count={count} radius={radius} customWords={customWords} />
+    </group>
+  );
+}
+
+interface WordCloud3DProps {
+  customWords?: string[];
+}
+
+export default function WordCloud3D({ customWords }: WordCloud3DProps) {
+  return (
+    <Canvas dpr={[1, 2]} camera={{ position: [0, 0, 40], fov: 70 }}>
+      <ambientLight intensity={0.5} />
+      <fog attach="fog" args={["#3b82f6", 20, 80]} />
+      <Suspense fallback={null}>
+        <RotatingGroup count={6} radius={20} customWords={customWords} />
+      </Suspense>
+      <TrackballControls />
+    </Canvas>
+  );
+}
